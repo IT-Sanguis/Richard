12b56420c3b0ad18d4b9b6c2b555bb0939df6f46
Sanguis <samus941@gmail.com>
2023-04-13 15:17:36 +0400

    Edited content, added font fallback

--- source/send.php
+++ source/send.php
@@ -12,14 +12,13 @@ if (!defined('TOKEN') || !defined('CHAT_ID')) {
 $token = TOKEN;
 $chat_id = CHAT_ID;
 
-
 $phone = preg_replace('/\D/', '', $_POST['phone']);
 if (substr($phone, 0) == '8') {
     $phone = '7'.substr($phone, 0);
 }
 // $phone = '+'.substr($phone, 0);
 
-//Определяем переменные для передачи данных из нашей формы
+
 if ($_POST['act'] == 'order') {
     $name = ($_POST['name']);
     $phone = '<a+href="tel:%2B'.$phone.'">%2B'.$phone.'</a>';
@@ -29,7 +28,6 @@ if ($_POST['act'] == 'order') {
     //     $email = ($_POST['email']);
     // }
 
-//Собираем в массив то, что будет передаваться боту
     $arr = array(
         'ID:' => uniqid(),
         'Имя:' => $name,
@@ -42,29 +40,18 @@ if ($_POST['act'] == 'order') {
     //     array_push($array, $email);
     // }
 
-//Настраиваем внешний вид сообщения в телеграме
-$txt = "";
+
+    $txt = "";
     foreach($arr as $key => $value) {
         $txt .= "<b>".$key."</b> ".$value."%0A";
     };
 
-//Передаем данные боту
     $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
-//Выводим сообщение об успешной отправке
     if ($sendToTelegram) {
         echo 'Спасибо! Ваша заявка принята. Мы свяжемся с вами в ближайшее время.';
-        // echo '<script>';
-        // echo 'const modalFeedbackT = document.querySelector(".modal.feedback");';
-        // echo 'const modalSuccessT = document.querySelector(".modal.success");';
-        // echo 'modalFeedbackT.classList.remove("modal--show");';
-        // echo 'modalSuccessT.classList.remove("modal--show");';
-        // echo 'modalSuccessT.offsetWidth = modalSuccessT.offsetWidth;';
-        // echo 'modalSuccessT.classList.add("modal--show");';
-        // echo '</script>';
     }
 
-//А здесь сообщение об ошибке при отправке
     else {
         echo 'Что-то пошло не так. Попробуйте отправить форму ещё раз.';
     }
